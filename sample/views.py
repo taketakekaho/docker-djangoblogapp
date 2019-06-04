8e7871cd6d003f4b6160435f2d813ea3781d2af6
taketakekaho <kahori_takeda@change-jp.com>
2019-06-04 21:13:26 +0900

    画面を作成

--- sample/views.py
+++ sample/views.py
@@ -1,7 +1,37 @@
-from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
+from django.views import generic
 
-# Create your views here.
-from django.http import HttpResponse
+from .models import Choice, Question
 
-def index(request):
-    return HttpResponse("Hello world.")
+class IndexView(generic.ListView):
+    template_name = 'sample/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by('-pub_date')[:5]
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'sample/detail.html'
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'sample/results.html'
+
+def vote(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        # Redisplay the question voting form.
+        return render(request, 'sample/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('sample:results', args=(question.id,)))
